5c942947375062233076683a3a1ae7ce09299d5f
johantorosjan <johan.torosjan@etu.umontpellier.fr>
2025-11-24 21:13:06 +0100

    save

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,38 @@
 // src/pages/Home.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logomain from "../assets/logo-main.png";
+import logomaintel from "../assets/tel.jpg";
 import fed from "../assets/fed.svg";
 
 import './Home.css';
 
-// src/pages/Home.jsx
 function Home() {
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="page-container">
       <div className="page-background"></div>
       <div className="page-content">
-                <img className="logo-center" src={logomain} alt="Mon illustration" />
-                <img className="logo-bottom-right" src={fed} alt="Mon illustration" />
-
+        <img
+          className="logo-center"
+          src={isMobile ? logomaintel : logomain}
+          alt="Mon illustration"
+        />
+        <img
+          className="logo-bottom-right"
+          src={fed}
+          alt="Mon illustration"
+        />
       </div>
     </div>
   );
