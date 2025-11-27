e8b11fe729d9ea2defdb8600400853ed322f3e85
johantorosjan <johan.torosjan@etu.umontpellier.fr>
2025-11-27 16:04:32 +0100

    after sommaire

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,11 @@
 // src/pages/Contact.js
 import React, { useState, useEffect } from 'react';
+import './Contacts.css'
+import contactsbg from "../assets/contactsbg.jpg";
+import ScrollToTopButton from '../components/ScrollToTopButton.jsx'
+import prod from '../assets/prod.png'
+import sales from '../assets/sale.png'
+import line from '../assets/line.png'
 
 function Contacts() {
             useEffect(() => {
@@ -7,10 +13,77 @@ function Contacts() {
                 top: 0,
               });})
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>Contactez-nous à contact@example.com</p>
-    </div>
+ <div className="contacts-container">
+<ScrollToTopButton/>
+
+      <div className="contacts-title">
+        <h1>Contacts</h1>
+      </div>
+      
+      <div className="contacts-hero" style={{ backgroundImage: `url(${contactsbg})` }}>
+      </div>
+      
+      <div className="contacts-content">
+      
+
+
+      <section className="contacts-section">
+            <div className='contacts-section-title'>
+    <img
+          className="imgthemes"
+          src={prod}
+          alt="loading"
+        />  
+
+            </div>
+                            <div className='line-div'>
+
+    <img
+          className="separator-item"
+          src={line}
+          alt="loading"
+        />  
+        
+        </div>
+
+        <div className='prod-text'>
+         <p>Pascal Breton – Founder and group CEO</p> 
+         <p>Judith ROCHELOIS - Producer</p>
+         <p>judith.rochelois@fedent.com </p>
+         <p>+33 6 22 06 71 59 </p>
+        </div>
+    </section>
+
+
+      <section className="contacts-section">
+            <div className='contacts-section-title'>
+    <img
+          className="imgthemes"
+          src={sales}
+          alt="loading"
+        />  
+
+            </div>
+                            <div className='line-div'>
+
+    <img
+          className="separator-item"
+          src={line}
+          alt="loading"
+        />  
+        
+        </div>
+
+        <div className='prod-text'>
+         <p>Guillaume Pommier – Co-chief of distribution</p> 
+         <p>guillaume.pommier@fedent.com</p>
+         <p> +33 6 46 35 41 57</p>
+        </div>
+    </section>
+
+      </div>
+
+  </div>
   );
 }
 
